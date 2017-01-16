1d76ef277dfb2cfa96e937149b90402bab3ec0d2
Wasswa Samuel <latimerscope@gmail.com>
2017-01-16 00:11:08 +0000

    Change flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         log_in @user
-        flash[:success] = "Welcome to the Sample App!"
+        flash[:success] = "Welcome to the Mastercard management system!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
